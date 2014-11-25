c45e42b0f3fa084fa3eeac40402964a08e304387
Martin Kamleithner <mkamleithner@localhost.localdomain>
2014-11-25 23:13:51 +0100

    Initial commit

---
+++ src/AsciiImage.java
@@ -0,0 +1,247 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Created by mkamleithner on 10/24/14.
+ */
+public class AsciiImage {
+
+    public static final String OPERATION_FAILED = "OPERATION FAILED";
+    //the ascii image as one whole string without newline-characters
+    private char[][] asciiImage;
+
+    /**
+     * creates a new, empty AsciiImage instance
+     */
+
+    public AsciiImage(){
+
+    }
+
+    /**
+     * creates an AsciiImage from a String[]
+     * @param lines
+     */
+    public AsciiImage(String[] lines){
+        this.asciiImage = new char[lines.length][];
+        for (int i = 0; i < lines.length ; i++) {
+            this.asciiImage[i]= lines[i].toCharArray();
+        }
+
+    }
+
+    public AsciiImage (int width, int height){
+       this.asciiImage = new char[width][height];
+       clear();
+    }
+
+    public void clear(){
+
+        for (int i = 0; i < this.asciiImage.length ; i++) {
+            Arrays.fill(this.asciiImage[i], '.');
+        }
+
+    }
+
+    public int getWidth(){
+        return asciiImage.length;
+    }
+
+    public int getHeight(){
+        return  asciiImage[0].length;
+    }
+
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        this.transpose();
+        for (int i = 0; i < this.asciiImage.length ; i++) {
+            builder.append(this.asciiImage[i]).append(System.lineSeparator());
+        }
+        this.transpose();
+        return builder.toString();
+
+    }
+
+    /**
+     * returns the character on position x/y.
+     * @param x
+     * @param y
+     * @return
+     * @throws AsciiShop.AsciiShopException if x/y are out of the boundaries of this image
+     */
+    public char getPixel(int x, int y){
+        return this.asciiImage[x][y];
+    }
+
+    /**
+     * sets the character on position x/y.
+     * @param x
+     * @param y
+     * @param c
+     * @throws AsciiShop.AsciiShopException if x/y are out of the boundaries of this image
+     */
+    public void setPixel(int x, int y, char c){
+        this.asciiImage[x][y] = c;
+    }
+
+    /**
+     * replaces the character on position x/y with c, and recursively all neighbour characters
+     * which do not match c.
+     * @param x the x coordinate
+     * @param y the y coordinate
+     * @param c that character which should replace the old char at x/y
+     * @throws AsciiShop.AsciiShopException in case of invalid x/y
+     */
+    public  void fill( int x, int y, char c) {
+        //check if this character already matches this character
+        // or throw exception in case of invalid x/y
+        if ((this.getPixel(x, y) == c)) {
+            //nothing to do
+            return;
+        }
+        //save the old character for later
+        char oldChar = this.getPixel(x, y);
+        //replace the char on position x/y with the desired character
+        this.setPixel(x, y, c);
+        //check left neighbour
+        if (x != 0 && getPixel(x - 1, y) == oldChar) {
+            fill(x - 1, y, c);
+        }
+        //check above neighbour
+        if (y != 0 && getPixel(x, y - 1) == oldChar) {
+            fill(x, y - 1, c);
+        }
+        //check right neighbour
+        if (x < this.getWidth() - 1 && getPixel(x + 1, y) == oldChar) {
+            fill( x + 1, y, c);
+        }
+        //check down neighbour
+        if (y < this.getHeight() - 1 && getPixel(x, y + 1) == oldChar) {
+            fill(x, y + 1, c);
+        }
+
+    }
+
+    /**
+     * transposes the image.
+     *
+     */
+    public void transpose(){
+        //create a new asciiimage with the dimensions of the old image, but width and height swapped
+        AsciiImage help = new AsciiImage(this.getHeight(), this.getWidth());
+
+        //get the char on position x,y on the original image and set it on the new image on position y/x
+        for (int x = 0 ; x < this.getWidth() ; x++){
+            for (int y = 0; y <this.getHeight(); y++){
+                help.setPixel(y, x, getPixel(x, y));
+            }
+        }
+        //take the state of the new image and copy it to new original image
+        this.asciiImage = help.asciiImage;
+
+
+    }
+
+    /**
+     * returns the lineIndex_th line. does not validate the input.
+     * @param lineIndex
+     * @return
+     * @throws java.lang.IndexOutOfBoundsException if lineIndex is not valid
+     */
+    private char[] getLine(int lineIndex){
+        return this.asciiImage[lineIndex];
+    }
+
+
+
+    /**
+     * returns, if every line is this image is a palindrome.
+     *
+     * @return true/false
+     */
+    public boolean isSymmetricHorizontal(){
+        for(int i= 0; i< this.getHeight() ; i++){
+            if(!this.isLineHorizontal(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * returns, if the line at lineIndex is a palindrome
+     * @param lineIndex
+     * @return
+     */
+    private boolean isLineHorizontal(int lineIndex){
+        char[] line = this.getLine(lineIndex);
+        int lineLength = line.length;
+        //only check to lineLength / 2 to avoid double-checks
+        //**.**
+        //01234
+        //element 0 is compared to element 4
+        //...     1                        3
+        //element 2 is not compared to anything.
+        for (int i = 0; i < lineLength /2 ; i++) {
+            if(line[i]!= line[(lineLength-1-i)]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public void replace(char oldChar, char newChar){
+        for (int i = 0; i < this.asciiImage.length; i++) {
+            for (int j = 0; j < this.asciiImage[i].length; j++) {
+                if(this.asciiImage[i][j] == oldChar){
+                    this.asciiImage[i][j] = newChar;
+                }
+            }
+        }
+    }
+
+    public void drawLine(int x0, int y0, int x1, int y1, char c){
+        System.out.println("drawline "+x0 +" "+ y0+" " + x1+" " +y1+" " + c);
+        boolean swapped = false;
+        int help;
+        int deltaX = x1-x0;
+        int deltaY = y1-y0;
+        //check if the axis are inverted
+        if(Math.abs(deltaY) > Math.abs(deltaX)){
+            swapped = true;
+            //swap all x with y
+            help = deltaX;
+            deltaX = deltaY;
+            deltaY = help;
+            help = x0;
+            x0 = y0;
+            y0= help;
+            help =x1;
+            x1 =y1;
+            y1 = help;
+
+        }
+        if(x1 < x0){
+            help= x0;
+            x0 =x1;
+            x1 = help;
+        }
+        double y = y0;
+        System.out.println("x0: " +x0 + " x1: "+x1+ " y0: "+ y0 + " y1: " + y1);
+        for (int x = x0  ; x <= x1; x++, y += (((double)deltaY)/deltaX)) {
+                if(swapped){
+                    this.asciiImage[(int)y][x] = c;
+               }else
+                    this.asciiImage[x][(int)y] = c;
+
+        }
+
+
+    }
+
+
+
+}
---
+++ src/AsciiShop.java
@@ -0,0 +1,340 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by mkamleithner on 10/24/14.
+ */
+public class AsciiShop {
+
+    /**
+     * generic error message
+     */
+    public static final String INPUT_ERROR = "INPUT MISMATCH";
+
+    public static void main(String[] args) {
+
+        try {
+            AsciiImage image = null;
+            Scanner scanner = new Scanner(System.in);
+
+
+            AsciiImageCommandBuilder builder = new AsciiImageCommandBuilder();
+            AsciiImageOperation operation = builder.buildOperation(scanner);
+            if(!(operation instanceof  AsciiImageCreator)){
+                throw new AsciiShopException(INPUT_ERROR);
+            }
+            image = ((AsciiImageCreator)operation).performTask(null);
+
+            //read lines until no more lines are available
+            while (scanner.hasNextLine()) {
+                //build operation-object from input-command
+                operation = builder.buildOperation(scanner);
+                //image has already been created, must not be created again.
+                if(operation instanceof  AsciiImageCreator){
+                    throw new AsciiShopException(INPUT_ERROR);
+                }
+
+                //perform that operation on the image
+                Object operationResult = operation.performTask(image);
+                //if this operation had a result, save it
+
+
+                if (operationResult != null) {
+                    System.out.println(operationResult);
+                }
+            }
+
+        } catch (AsciiShopException ase) {
+            //print error message
+            System.out.println(ase.getMessage());
+        }
+    }
+
+    /**
+     * interface for an operation to perform on an ascii-image
+     *
+     * @param <T> Result of the operation. May be Void.
+     */
+    public static interface AsciiImageOperation<T> {
+        public T performTask(AsciiImage image);
+    }
+
+    /**
+     * class to read commands, parse them and convert them to Operation-Objects
+     */
+    public static class AsciiImageCommandBuilder {
+
+        public AsciiImageOperation buildOperation(Scanner input) {
+            String line = input.nextLine();
+            String[] token = line.split(" ");
+            if("create".equals(token[0])){
+                if(token.length != 3){
+                    throw new AsciiShopException(INPUT_ERROR);
+                }
+                int width, height;
+                width= Integer.parseInt(token[1]);
+                height= Integer.parseInt(token[2]);
+                return new AsciiImageCreator(width,height);
+            }
+            else if("load".equals(token[0])) {
+                if(token.length != 2){
+                    throw new AsciiShopException(INPUT_ERROR);
+                }
+                return new AsciiImageLoader(token[1], input);
+            }else if("print".equals(token[0])){
+                return new AsciiPrintTask();
+            }else if ("clear".equals(token[0])) {
+                return new AsciiClearTask();
+            }else if ("replace".equals(token[0])) {
+                if(token.length != 3 && (token[1].length() != 1  || token[2].length() != 1)){
+                    throw new AsciiShopException(INPUT_ERROR);
+                }
+                char oldChar = token[1].charAt(0);
+                char newChar = token[2].charAt(0);
+
+                return new AsciiReplaceOperation(oldChar,newChar);
+
+
+            }else if("line".equals(token[0])){
+                if(token.length != 6){
+                    throw new AsciiShopException(INPUT_ERROR);
+                }
+                int x0, y0, x1, y1;
+                char c;
+                //try to parse the parameters, throw an exception otherwise
+                //x,y must be integers, checking of the boundaries is up to the AsciiImage itself
+                try {
+                    x0 = Integer.parseInt(token[1]);
+                    y0 = Integer.parseInt(token[2]);
+                    x1 = Integer.parseInt(token[3]);
+                    y1 = Integer.parseInt(token[4]);
+                } catch (NumberFormatException nfe) {
+                    throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+                }
+                //c must be exactly one character
+                if (token[5].length() != 1) {
+                    throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+                }
+                c = token[5].charAt(0);
+                return new AsciiLineDrawer(x0,y0,x1,y1,c);
+            }
+            if ("transpose".equals(token[0])) {
+                if (token.length != 1) {
+                    throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+                }
+                return new Transposer();
+            } else if ("fill".equals(token[0])) {
+                if (token.length != 4) {
+                    throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+                }
+                //parameters for the fill command
+                int x, y;
+                char c;
+                //try to parse the parameters, throw an exception otherwise
+                //x,y must be integers, checking of the boundaries is up to the AsciiImage itself
+                try {
+                    x = Integer.parseInt(token[1]);
+                    y = Integer.parseInt(token[2]);
+                } catch (NumberFormatException nfe) {
+                    throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+                }
+                //c must be exactly one character
+                if (token[3].length() != 1) {
+                    throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+                }
+                c = token[3].charAt(0);
+                return new AsciiImageFiller(x, y, c);
+            } else if ("symmetric-h".equals(token[0])) {
+                if (token.length != 1) {
+                    throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+                }
+                return new AsciiSymmetryChecker();
+            } else throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+        }
+
+
+    }
+
+    public static class AsciiImageCreator implements  AsciiImageOperation<AsciiImage> {
+
+        private final int width, height;
+
+        public AsciiImageCreator(int width, int height){
+            this.height = height;
+            this.width = width;
+        }
+
+        public AsciiImage performTask(AsciiImage image) {
+
+            if(image != null){
+                return null;
+            }
+
+            return new AsciiImage(width,height);
+        }
+    }
+
+    public static class AsciiImageLoader implements  AsciiImageOperation<AsciiImage> {
+
+
+
+        private final String eof;
+        private final Scanner scanner;
+
+        public AsciiImageLoader(String eof, Scanner input) {
+            this.eof = eof;
+            this.scanner = input;
+        }
+
+        public AsciiImage performTask(AsciiImage image) {
+
+            int width = image.getWidth();
+            int height = image.getHeight();
+
+            for (int i = 0; i < height; i++) {
+                if (!scanner.hasNextLine()) {
+                    throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+                }
+                String line = scanner.nextLine();
+                if (line.length() != width) {
+                    throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+                }
+                for (int j = 0; j < line.length(); j++) {
+                    image.setPixel(j, i, line.charAt(j));
+                }
+            }
+
+            if (!scanner.hasNextLine() || !scanner.nextLine().equals(eof)) {
+                throw new AsciiShopException(AsciiShop.INPUT_ERROR);
+            }
+
+
+            return image;
+        }
+    }
+
+
+    public static class AsciiClearTask implements  AsciiImageOperation<Void> {
+        public Void performTask(AsciiImage image) {
+            image.clear();
+            return null;
+        }
+    }
+
+    public static class AsciiPrintTask implements  AsciiImageOperation<Void> {
+        public  Void performTask(AsciiImage image) {
+            System.out.println(image.toString());
+            return null;
+        }
+    }
+
+
+
+    public static class AsciiSymmetryChecker implements AsciiImageOperation<Boolean> {
+
+        public Boolean performTask(AsciiImage image) {
+            return image.isSymmetricHorizontal();
+        }
+    }
+
+    public static class AsciiImageFiller implements AsciiImageOperation<Void> {
+
+
+        private int x, y;
+        private char character;
+
+
+        public AsciiImageFiller(int x, int y, char character) {
+            this.x = x;
+            this.y = y;
+            this.character = character;
+        }
+
+        public AsciiImageFiller() {
+
+        }
+
+        public AsciiImageFiller setX(int x) {
+            this.x = x;
+            return this;
+        }
+
+        public AsciiImageFiller setY(int y) {
+            this.y = y;
+            return this;
+        }
+
+        public AsciiImageFiller setCharacter(char character) {
+            this.character = character;
+            return this;
+        }
+
+        public Void performTask(AsciiImage image) {
+            image.fill(x, y, character);
+            return null;
+        }
+    }
+
+    public static class Transposer implements AsciiImageOperation<Void> {
+
+        public Void performTask(AsciiImage image) {
+
+            image.transpose();
+            return null;
+        }
+    }
+
+    public static class AsciiReplaceOperation implements  AsciiImageOperation<Void>{
+
+        char oldChar, newChar;
+
+        public AsciiReplaceOperation(char oldChar, char newChar)
+        {
+            this.oldChar = oldChar;
+            this.newChar = newChar;
+        }
+
+        @Override
+        public Void performTask(AsciiImage image) {
+            image.replace(oldChar, newChar);
+            return null;
+        }
+    }
+
+    public static class AsciiLineDrawer implements  AsciiImageOperation<Void>{
+
+        private int x0, x1,y0,y1;
+        private char c;
+
+        public AsciiLineDrawer(int x0, int y0, int x1, int y1, char c) {
+            this.x0 = x0;
+            this.x1 = x1;
+            this.y0 = y0;
+            this.y1 = y1;
+            this.c = c;
+        }
+
+        @Override
+        public Void performTask(AsciiImage image) {
+            image.drawLine(x0, y0, x1,y1,c);
+            return null;
+        }
+    }
+
+
+    /**
+     * own exception used for this project
+     */
+    public static class AsciiShopException extends RuntimeException {
+
+        public AsciiShopException() {
+
+        }
+
+        public AsciiShopException(String message) {
+            super(message);
+        }
+    }
+
+}
